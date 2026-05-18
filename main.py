8e53742f4caa8a8f18cbc27a50dcf6f8cc74ec39
Robosword <32asingh@stratfordschools.net>
2026-05-17 20:24:14 -0700

    Get the x, the y, and make the model.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import torch.nn as nn
+import torch
 
 dataset = pd.read_csv('cardio_data.csv', sep=';')
-print(dataset.head())
 
+X = dataset.drop(labels=["id", "cardio"])
+y = dataset["cardio"]
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(11, 16)
+        self.fc2 = nn.Linear(16, 8)
+        self.out = nn.Linear(8, 2)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.out(x)
+        return x
