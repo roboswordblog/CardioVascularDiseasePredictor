51296ab3b3243a56d7f488c770aa0db72e42a7bb
Robosword <32asingh@stratfordschools.net>
2026-05-17 21:38:02 -0700

    MAKING IT BETTER BETTER BETTER DIFFERENT TRAINING DIFFERENT SPLITTING MORE LAYER

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import torch.nn as nn
 import torch
 from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, DataLoader
+
 
 df = pd.read_csv('cardio_data.csv', sep=';')
 df["age_years"] = df["age"] / 365.25
@@ -18,41 +20,47 @@ X = df.drop(labels=["id", "cardio"], axis=1)
 y = df["cardio"]
 X = torch.FloatTensor(X.values)
 Y = torch.FloatTensor(y.values)
-X = (X - X.mean(dim=0)) / X.std(dim=0)
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(17, 16)
-        self.fc2 = nn.Linear(16, 8)
+        self.fc1 = nn.Linear(17, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 8)
         self.out = nn.Linear(8, 2)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
         x = self.out(x)
         return x
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.2, shuffle=True, random_state=42
+)
 
+mean = X_train.mean(dim=0)
+std = X_train.std(dim=0)
+
+X_train = (X_train - mean) / std
+X_test = (X_test - mean) / std
 y_train = y_train.long()
 y_test = y_test.long()
 model = Model()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.00125)
-epochs = 50000
+train_ds = TensorDataset(X_train, y_train)
+loader = DataLoader(train_ds, batch_size=512, shuffle=True)
+for epoch in range(45000):
+    for xb, yb in loader:
+        pred = model(xb)
+        loss = criterion(pred, yb)
 
-for i in range(epochs):
-    y_pred = model(X_train)
-    loss = criterion(y_pred, y_train)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    if i % 1000 == 0:
-        predictions = torch.argmax(y_pred, dim=1)
-        accuracy = (predictions == y_train).float().mean()
-        print(accuracy)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
 with torch.no_grad():
     model.eval()
